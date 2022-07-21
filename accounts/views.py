a834935f269106dc05c1d0d3fc5d56c0f1bff81f
bplong33 <bplong96@gmail.com>
2022-07-21 09:45:11 -0400

    Added Index and UserList template views and linked in url conf

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,18 @@
+from itertools import tee
 from django.shortcuts import render
+from .models import User
+from django.views.generic import TemplateView
 
 # Create your views here.
+class Index(TemplateView):
+    template_name = "index.html"
+
+class UserList(TemplateView):
+    template_name = r"accounts\users.html"
+    
+    # def get_context_data(self, **kwargs: Any) -> Dict[str, Any]:
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        data = User.objects.all()
+        context.update({'users':data})
+        return context
--- lvl2_proj/urls.py
+++ lvl2_proj/urls.py
@@ -14,8 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from accounts.views import Index
 
 urlpatterns = [
+    path('', Index.as_view(), name='index'),
+    path('users/', include('accounts.urls')),
     path('admin/', admin.site.urls),
 ]
